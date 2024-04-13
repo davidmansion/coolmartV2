30434e77802395eb97d7a7455e982da7115c80e2
davidmansion <contact@davidmansion.com>
2024-04-13 20:04:02 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-  basePath: "",
 };
 
 module.exports = nextConfig;
