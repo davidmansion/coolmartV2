ce8d6a0ae9621fe2aa722cbda522aefc130590bb
davidmansion <93778907+davidmansion@users.noreply.github.com>
2024-04-15 14:45:28 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Navbar />
       <Component {...pageProps} />
       </ThirdwebProviderV5>
-    </ThirdwebProvider>
+    </V4TWProvider>
   );
 }
 
